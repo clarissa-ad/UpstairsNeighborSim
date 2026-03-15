4d8e3d5c377252586abda8523b925bac2ddee455
Clarissa A <clarissaadit@gmail.com>
2026-03-15 15:54:45 +0700

    Initial Commit

---
+++ UpstairsNeighborSim/UpstairsNeighborSimApp.swift
@@ -0,0 +1,17 @@
+//
+//  UpstairsNeighborSimApp.swift
+//  UpstairsNeighborSim
+//
+//  Created by Clarissa Aditjakra on 15/03/26.
+//
+
+import SwiftUI
+
+@main
+struct UpstairsNeighborSimApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ UpstairsNeighborSimTests/UpstairsNeighborSimTests.swift
@@ -0,0 +1,16 @@
+//
+//  UpstairsNeighborSimTests.swift
+//  UpstairsNeighborSimTests
+//
+//  Created by Clarissa Aditjakra on 15/03/26.
+//
+
+import Testing
+
+struct UpstairsNeighborSimTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
